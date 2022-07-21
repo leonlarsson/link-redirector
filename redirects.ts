8b7bbdb656d350898230d780fbcb3a9bf09c5d9e
Leon San José Larsson <leonlarsson8@gmail.com>
2022-07-21 18:15:45 +0200

    update redirects

--- redirects.ts
+++ redirects.ts
@@ -1,6 +1,13 @@
 export default {
     "/home": "https://leonlarsson.com",
+    "/gh": "https://github.com/leonlarsson",
+    "/li": "https://www.linkedin.com/in/leonlarsson",
+    "/tw": "https://twitter.com/MozzyFX",
+
     "/ls": "https://logsorter.net",
+    "/morse": "https://bf1morse.leonlarsson.com",
     "/stats": "https://bfstats.leonlarsson.com",
-    "/trash": "https://api.onlyraccoons.com"
+    "/trash": "https://api.onlyraccoons.com",
+    "/joy": "https://joy.leonlarsson.com",
+    "/bfvmenu": "https://bfvmenu.leonlarsson.com/",
 };
