825c51fd2270ae191a9462c47b4d393ad2302d49
Leon San José Larsson <leonlarsson8@gmail.com>
2022-12-19 16:40:11 +0100

    change redirect structure to use path arrays
    This allows for multiple aliases.

--- config.ts
+++ config.ts
@@ -1,56 +1,71 @@
 type Config = {
+    /** The title of the page. Shown in meta and on page. */
     title?: string;
+    /** The description of the page. Shown in meta and on page. Omitting will remove the description on the page. */
     description?: string;
-    redirects: Record<string, Link>
+    /** An array of Links (redirect objects). */
+    redirects: Link[];
 };
 
 type Link = {
+    /** An array of paths you want to match. */
+    paths: string[];
+    /** The URL to redirect to. */
     url: string;
+    /** Whether or not to show in Home. */
     showInHome?: boolean;
+    /** The FontAwesome Icon classes. Omitting = no icon. You should specify at least one of icon or name. */
     icon?: string;
+    /** The name of the button. Omitting = no name. You should specify at least one of icon or name. */
     name?: string;
 };
+
+// If showInHome is true, you should specify at least icon OR home. Include both to display both name and icon. Omitting both will display an empty button, which does not look good...
 
 export default {
     title: "Leon's Links",
     description: "A collection of useful links where you can connect with me.",
-    redirects: {
-        "/home": {
+    redirects: [
+        {
+            paths: ["/home"],
             url: "https://leonlarsson.com",
             showInHome: true,
             icon: "fa-solid fa-house-user",
-            name: "Home",
+            name: "Home"
         },
-        "/projects": {
+        {
+            paths: ["/projects", "/p"],
             url: "https://leonlarsson.com/projects",
             showInHome: true,
             icon: "fa-solid fa-hammer",
             name: "Projects"
         },
-        "/gh": {
+        {
+            paths: ["/gh"],
             url: "https://github.com/leonlarsson",
             showInHome: true,
             icon: "fa-brands fa-github",
             name: "GitHub"
         },
-        "/li": {
+        {
+            paths: ["/li"],
             url: "https://www.linkedin.com/in/leonlarsson",
             showInHome: true,
             icon: "fa-brands fa-linkedin",
             name: "LinkedIn"
         },
-        "/tw": {
+        {
+            paths: ["/tw"],
             url: "https://twitter.com/MozzyFX",
             showInHome: true,
             icon: "fa-brands fa-twitter",
             name: "Twitter"
         },
-        "/p": { url: "https://leonlarsson.com/projects" },
-        "/ls": { url: "https://logsorter.net" },
-        "/morse": { url: "https://bf1morse.leonlarsson.com" },
-        "/stats": { url: "https://bfstats.leonlarsson.com" },
-        "/trash": { url: "https://api.onlyraccoons.com" },
-        "/joy": { url: "https://joy.leonlarsson.com" },
-        "/bfvmenu": { url: "https://bfvmenu.leonlarsson.com/" }
-    }
+        { paths: ["/ls"], url: "https://logsorter.net" },
+        { paths: ["/morse"], url: "https://bf1morse.leonlarsson.com" },
+        { paths: ["/stats"], url: "https://bfstats.leonlarsson.com" },
+        { paths: ["/trash"], url: "https://api.onlyraccoons.com" },
+        { paths: ["/joy"], url: "https://joy.leonlarsson.com" },
+        { paths: ["/bfvmenu"], url: "https://bfvmenu.leonlarsson.com/" }
+    ]
 } as Config;
--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import home from "./home";
 import config from "./config";
-const paths = Object.keys(config.redirects);
 
 export default {
     fetch(request: Request) {
         const requestPath = new URL(request.url).pathname;
-        return paths.includes(requestPath) ? Response.redirect(config.redirects[requestPath].url, 302) : home(requestPath);
+        const requestedLink = config.redirects.find(x => x.paths.includes(requestPath));
+        return requestedLink ? Response.redirect(requestedLink.url, 302) : home(requestPath);
     }
 }
